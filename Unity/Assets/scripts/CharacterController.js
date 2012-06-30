9ce00dae752ade0cb1c5ec6ce0059ec0a79c5209
Alex <alexander.ebel90@gmx.de>
2012-06-30 19:10:59 +0200

    änderungen

--- Unity/Assets/scripts/CharacterController.js
+++ Unity/Assets/scripts/CharacterController.js
@@ -9,23 +9,29 @@ function Start () {
 
 function Update () {
 	
+	if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow)){
+		gameObject.rigidbody.velocity.x = 0;
+	}
 	if (Input.GetKey(KeyCode.LeftArrow)){
 	
 		/*actPos +=  Vector3(0,0,-200) * Time.deltaTime ;
 		gameObject.rigidbody.MoveRotation(Quaternion.Euler(actPos));	
 		gameObject.rigidbody.MovePosition(gameObject.rigidbody.position+Vector3(3,0,0) * Time.deltaTime);*/
 		if (gameObject.rigidbody.velocity.x < 0 &&  lastKey == KeyCode.RightArrow){
-			gameObject.rigidbody.AddForce(Vector3(-gameObject.rigidbody.velocity.x,0,0),ForceMode.VelocityChange);
+			//gameObject.rigidbody.AddForce(Vector3(-gameObject.rigidbody.velocity.x,0,0),ForceMode.VelocityChange);
+			gameObject.rigidbody.velocity.x = 0;
 		}
 				
 		if (Input.GetKey(KeyCode.LeftShift)){
 			
-			gameObject.rigidbody.AddForce( Vector3(20,0,0) * Time.deltaTime,ForceMode.VelocityChange);
+			//gameObject.rigidbody.AddForce( Vector3(20,0,0) * Time.deltaTime,ForceMode.VelocityChange);
+			gameObject.rigidbody.velocity.x = 10;
 		}
 		else {
-			gameObject.rigidbody.AddForce(Vector3(10,0,0) * Time.deltaTime,ForceMode.VelocityChange);
+			//gameObject.rigidbody.AddForce(Vector3(10,0,0) * Time.deltaTime,ForceMode.VelocityChange);
+			gameObject.rigidbody.velocity.x = 5;
 		}
-		print(gameObject.rigidbody.velocity);
+		//print(gameObject.rigidbody.velocity);
 		lastKey = KeyCode.LeftArrow;
 	}
 	if (Input.GetKey(KeyCode.RightArrow)){
@@ -34,19 +40,27 @@ function Update () {
 		gameObject.rigidbody.MovePosition(gameObject.rigidbody.position+Vector3(-3,0,0) * Time.deltaTime);	
 		*/
 		if (gameObject.rigidbody.velocity.x > 0 && lastKey == KeyCode.LeftArrow){
-			gameObject.rigidbody.AddForce(Vector3(-gameObject.rigidbody.velocity.x,0,0),ForceMode.VelocityChange);
+			//gameObject.rigidbody.AddForce(Vector3(-gameObject.rigidbody.velocity.x,0,0),ForceMode.VelocityChange);
+			gameObject.rigidbody.velocity.x = 0;
 		}
-		if (Input.GetKey(KeyCode.LeftShift)){
-			gameObject.rigidbody.AddForce(Vector3(-20,0,0) * Time.deltaTime,ForceMode.VelocityChange);
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			gameObject.rigidbody.velocity.x = 0;
+		}else{
+			if (Input.GetKey(KeyCode.LeftShift)){
+				//gameObject.rigidbody.AddForce(Vector3(-20,0,0) * Time.deltaTime,ForceMode.VelocityChange);
+				gameObject.rigidbody.velocity.x = -10;
+			}
+			else {
+				//gameObject.rigidbody.AddForce(Vector3(-10,0,0) * Time.deltaTime,ForceMode.VelocityChange);
+				gameObject.rigidbody.velocity.x = -5;
+			}
 		}
-		else {
-			gameObject.rigidbody.AddForce(Vector3(-10,0,0) * Time.deltaTime,ForceMode.VelocityChange);
-		}
-		print(gameObject.rigidbody.velocity);
+		//print(gameObject.rigidbody.velocity);
 		lastKey = KeyCode.RightArrow;
 	}
 	if (Input.GetKeyDown(KeyCode.Space)){
-		gameObject.rigidbody.AddForce(Vector3(0,9,0),ForceMode.VelocityChange);
+		//print(gameObject.rigidbody.velocity);
+		gameObject.rigidbody.AddForce(Vector3(0,8,0),ForceMode.VelocityChange);
 	}
-	
+	print(gameObject.rigidbody.velocity);
 }
